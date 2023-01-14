c0f995510ace05ec28e52a6f4314ae342c4a2b3e
Jeb <jesperkauppinen@gmail.com>
2023-01-14 15:09:43 +0200

    prduction db

--- api/database.py
+++ api/database.py
@@ -2,10 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+from os import getenv
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite"
+SQLALCHEMY_DATABASE_URL = getenv("DATABASE_URL") or "postgresql://user:passwd@db:5432/table"
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": True}) # sqlite specific argument
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
